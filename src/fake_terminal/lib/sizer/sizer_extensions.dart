f8ed861e1abecf228c1f841c458885d62d75529b
nfdz <noe.fernandez@protonmail.com>
2021-08-15 12:45:13 +0100

    WIP

--- src/fake_terminal/lib/sizer/sizer_extensions.dart
+++ src/fake_terminal/lib/sizer/sizer_extensions.dart
@@ -1,10 +1,7 @@
 import 'dart:math' as Math;
 
 extension SizerX on double {
-  double withMaxValue(double maxValue) {                                                     return Math.min(this, maxValue);
-
-
-
-    
+  double withMaxValue(double maxValue) {
+    return Math.min(this, maxValue);
   }
 }
